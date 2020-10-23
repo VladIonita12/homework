5ad0a7b48bdbb917f1c60cd7741aade0e150b38c
vlad.ionita <vladdragos.ionita12@gmail.com>
2020-10-23 10:16:17 +0300

    8th commit

--- src/components/Account/insta-account.js
+++ src/components/Account/insta-account.js
@@ -3,24 +3,16 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import UserPachetStore from '../../store/UserPachetStore'
 import AccountStore from '../../store/AccountStore'
-import Form from 'react-bootstrap/Form'
-class AccountComponent extends Component{
+class InstaAccount extends Component{
 
     constructor(props){
         super(props)
         this.state={
             userPachet: null,
-            accArr:[],
-            instaAcc:[]
 
         }    
         this.store = new UserPachetStore();
         this.store1 = new AccountStore();
-        this.handleChange = (evt) => {
-            this.setState({
-                [evt.target.name]: evt.target.value
-            })
-        }
     }
     componentDidMount(){
         this.store.getUserPacket(this.props.account.id)
@@ -44,17 +36,10 @@ class AccountComponent extends Component{
             <p>hello</p>
             <h1>{this.props.account.numeClient}</h1>
             <h2>{this.state.userPachet}</h2>
-        {Array.from(Array(this.state.userPachet)).map((x, index) =>     
-   <div> <Form.Group  controlId="formBasicEmail">
-   <Form.Label>{index}. Instagram Account</Form.Label>
-    <Form.Control value={this.state.instaAcc[index]} key={index} name="instaAcc" placeholder="Enter Account" onChange={this.handleChange}  />
-    </Form.Group>
-    <Button onClick={this.state.accArr[index]=this.state.instaAcc[index]} >Save</Button>
-   </div>
-        )}
+
 
         </div>
         )
-        }
+    }
 }
-export default AccountComponent
+export default InstaAccount
